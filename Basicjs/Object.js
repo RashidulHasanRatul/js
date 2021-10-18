6cec2663e30f2397f3f3ff937ad2ba2045be66c5
Rashidul Hasan <rhratul12@gmail.com>
2021-10-18 18:46:55 +0600

    static dropdown

--- Basicjs/Object.js
+++ Basicjs/Object.js
@@ -15,3 +15,35 @@ let car={
 console.log(
     car.fulladd()
 );
+
+console.log("Hey This is Bangladesh");
+
+// 1 to 6 return 
+
+function rand(start,end) {
+    
+    let sum = Math.floor(Math.random()*(start-end+1))+start;
+    return sum;
+}
+
+console.log(rand(1,6));
+
+
+// Map example 
+
+let apple = new Map();
+apple.set(
+    NameofFruits,"Apple",
+    SerialNumber,"1122"
+
+)
+
+console.log(apple);
+
+let names = {
+
+    fname:"Ratul",
+    lname:'Hasan'
+}
+
+console.log(names);
---
+++ Basicjs/input.js
@@ -0,0 +1,13 @@
+function inputnumber(num) {
+    
+if(num==80){
+   console.log("A+");
+} else if(num==70){
+    console.log("A-");
+} else {
+    console.log("Wrong Entry");
+}
+
+}
+
+inputnumber(70);
