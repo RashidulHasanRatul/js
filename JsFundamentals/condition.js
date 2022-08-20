8e1e12099241fc421bff9d4b97582ccca1544a2e
Rashidul Hasan <rhratul12@gmail.com>
2022-08-21 02:08:21 +0600

    Added Code for Js Fundamentals

---
+++ JsFundamentals/condition.js
@@ -0,0 +1,16 @@
+let msg = 10 > 90 ? "yes" : 99 > 100 ? "no" : "maybe";
+console.log(msg);
+
+let test2 = 10 > 90 ? "yes" : "no";
+console.log(test2);
+
+let login = "Director";
+let message =
+  (login == "Emp")
+    ? "hello"
+    : login == "Director"
+    ? "Greetings"
+    : login == ""
+    ? "no login"
+    : "";
+console.log(message);
---
+++ JsFundamentals/typeConversion.js
@@ -0,0 +1,6 @@
+let number = 123;
+console.log(typeof number);
+
+let num = String(number);
+console.log(num);
+console.log(typeof num);
